f0cdd83c500dfd73cb6b9c638d572162d9edced4
Victor <victorv24@gmail.com>
2021-04-05 19:30:03 -0700

    Added boilerplate code.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Calculator should be able to:
+//   - Add two numbers
+//   - Subtract two numbers
+//   - Multiply two numbers
+//   - Divide two numbers
+//   - Display at least 10 digits on the screen
+//   - Display decimals to at least the thousandth (.001)
+//   - Perform continuous functions
+//       Example:
+//         Press the 1 button
+//         Press the add button
+//         Press the 1 button
+//         Press the add button again (result should show as 2 on the screen)
+//         Press the 1 button
+//         Press the add button again (result should show as 3 on the screen)
+//   - Pressing the clear button resets screen to 0
