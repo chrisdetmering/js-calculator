2ed6be87dcb72686c000b0dabcde1d2469ead341
Victor <victorv24@gmail.com>
2021-04-08 20:44:39 -0700

    Created class object and began working on logic flow.

--- js/script.js
+++ js/script.js
@@ -14,3 +14,38 @@
 //         Press the 1 button
 //         Press the add button again (result should show as 3 on the screen)
 //   - Pressing the clear button resets screen to 0
+
+class CalculationMachine {
+  constructor(userStartingNumber) {
+    this.userStartingNumber = userStartingNumber;
+  }
+
+  multiply(multiplier, ...theArgs) {
+    return theArgs.map((element) => {
+      return multiplier * element;
+    });
+  }
+}
+
+let calc = new CalculationMachine(2);
+console.log(calc.multiply(2, 2, 3, 4));
+
+// const adding = (adder, ...theArgs) => {
+//   return theArgs.map((element) => {
+//     return adder + element;
+//   });
+// }
+
+// const multiply = (multiplier, ...theArgs) => {
+// 	return theArgs.map((element) => {
+// 		return multiplier * element;
+// 	});
+// }
+
+// user clicks on a number button
+// and it is stored in an array
+
+// once the user clicks "="
+// the array is used to perform the
+// operation using the stored numbers
+// from the array. (using the spread operator)
