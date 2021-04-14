95e1a624abafc0506bb2899470c642699a79e6fd
Victor <victorv24@gmail.com>
2021-04-13 20:36:26 -0700

    Can perform a continuos function for adding numbers.

--- js/script.js
+++ js/script.js
@@ -1,57 +1,44 @@
-// Calculator should be able to:
-//   - Add two numbers
-//   - Subtract two numbers
-//   - Multiply two numbers
-//   - Divide two numbers
-//   - Display at least 10 digits on the screen
-//   - Display decimals to at least the thousandth (.001)
-//   - Perform continuous functions
-//       Example:
-//         Press the 1 button
-//         Press the add button
-//         Press the 1 button
-//         Press the add button again (result should show as 2 on the screen)
-//         Press the 1 button
-//         Press the add button again (result should show as 3 on the screen)
-//   - Pressing the clear button resets screen to 0
-
+// for testing
 const print = (text, output) => console.log(`${text}: ${output}`);
 
-const calcDisplay = document.getElementById("calc-display");
+const calcDisplay = document.getElementById('calc-display');
+const clearButton = document.getElementById('clear');
+const numberButtonContainer = document.querySelectorAll(
+	'#number-button-container'
+);
+const operatorButtonContainer = document.querySelectorAll(
+	'#operator-button-container'
+);
 
-const addNumbers = (...nums) => {
-  console.log(
-    nums.reduce((acc, currValue) => {
-      return acc + currValue;
-    })
-  );
+const numberStorageArray = [];
+
+numberButtonContainer.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		const buttonValue = e.target.textContent;
+		numberStorageArray.push(parseInt(buttonValue));
+
+		displayNumbers(buttonValue);
+	});
+});
+
+operatorButtonContainer.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		const buttonValue = e.target.textContent;
+
+		performOperation(buttonValue);
+	});
+});
+
+const displayNumbers = (numberValue) => (calcDisplay.innerText += numberValue);
+
+const performOperation = (operator) => {
+	calcDisplay.innerText = '';
+
+	if (operator === '=') {
+		calcDisplay.innerText = numberStorageArray.reduce((a, b) => a + b, 0);
+	}
 };
 
-const numbers = [];
-
-const initiateCalculation = (e) => {
-  const buttonValue = e.target.value;
-
-  if (
-    buttonValue !== undefined &&
-    buttonValue !== "+" &&
-    buttonValue !== "-" &&
-    buttonValue !== "X" &&
-    buttonValue !== "/" &&
-    buttonValue !== "CE" &&
-    buttonValue !== "="
-  ) {
-    print("Button clicked", buttonValue);
-    calcDisplay.innerText = e.target.value;
-    numbers.push(parseInt(buttonValue));
-    print("Numbers Array", numbers);
-  } else if (buttonValue === "=") {
-    const add = numbers[0] + numbers[1];
-    print("Add", add);
-    calcDisplay.innerText = add;
-  }
+const addNumbers = (a, b) => {
+	return a + b;
 };
-
-const checkButtonClick = document
-  .getElementById("calc-buttons-container")
-  .addEventListener("click", initiateCalculation);
