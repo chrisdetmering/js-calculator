d75183cfd7a32351e243bf36b06dad066918d7e8
Victor <victorv24@gmail.com>
2021-04-14 19:40:36 -0700

    Adds functionality for all math operations.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ const print = (text, output) => console.log(`${text}: ${output}`);
 
 const calcDisplay = document.getElementById('calc-display');
 const clearButton = document.getElementById('clear');
+
 const numberStorageArray = [];
+let currentOperator = null;
 
 document.querySelectorAll('#number-button-container').forEach((button) => {
 	button.addEventListener('click', (e) => {
 		const buttonValue = e.target.textContent;
 		numberStorageArray.push(parseInt(buttonValue));
-
 		displayNumbers(buttonValue);
 	});
 });
@@ -17,7 +18,9 @@ document.querySelectorAll('#number-button-container').forEach((button) => {
 document.querySelectorAll('#math-operator-container').forEach((button) => {
 	button.addEventListener('click', (e) => {
 		const buttonValue = e.target.textContent;
-
+		if (buttonValue !== '=') {
+			currentOperator = buttonValue;
+		}
 		beginCalculation(buttonValue);
 	});
 });
@@ -28,6 +31,14 @@ const beginCalculation = (operator) => {
 	calcDisplay.innerText = '';
 
 	if (operator === '=') {
-		calcDisplay.innerText = numberStorageArray.reduce((a, b) => a + b, 0);
+		if (currentOperator === '+') {
+			calcDisplay.innerText = numberStorageArray.reduce((a, b) => a + b);
+		} else if (currentOperator === '-') {
+			calcDisplay.innerText = numberStorageArray.reduce((a, b) => a - b);
+		} else if (currentOperator === 'X') {
+			calcDisplay.innerText = numberStorageArray.reduce((a, b) => a * b);
+		} else if (currentOperator === '/') {
+			calcDisplay.innerText = numberStorageArray.reduce((a, b) => a / b);
+		}
 	}
 };
