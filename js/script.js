410ea6556648f2fe5dee9e0f2f55e8a718e479fa
Victor <victorv24@gmail.com>
2021-04-17 11:51:16 -0700

    Adds switch statement

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// TO DO:
-// - Be able to store a multi-digit number, like "44"
-
-// user hits multiple numbers
-// user hits a math operator button
-// total number displayed is stored
-// if user hits equals, then numbers are calculated
-
 const calcDisplay = document.getElementById('calc-display');
 
 let numberStorageArray = [];
@@ -15,20 +7,22 @@ document.querySelectorAll('#number-button-container').forEach((button) => {
 	button.addEventListener('click', (e) => {
 		const buttonValue = e.target.textContent;
 		displayNumbers(buttonValue);
+		// debugger;
 	});
 });
 
 document.querySelectorAll('#math-operator-container').forEach((button) => {
 	button.addEventListener('click', (e) => {
 		const buttonValue = e.target.textContent;
-		debugger;
-		numberStorageArray.push(parseInt(calcDisplay.innerText));
+
+		numberStorageArray.push(parseFloat(calcDisplay.innerText));
 
 		if (buttonValue !== '=' && buttonValue !== 'CE') {
 			currentOperator = buttonValue;
 		}
 
 		beginCalculation(buttonValue);
+		// debugger;
 	});
 });
 
@@ -42,17 +36,32 @@ document.getElementById('clear').addEventListener('click', (e) => {
 const displayNumbers = (numberValue) => (calcDisplay.innerText += numberValue);
 
 const beginCalculation = (operator) => {
+	// debugger;
 	calcDisplay.innerText = '';
 
 	if (operator === '=') {
-		if (currentOperator === '+') {
-			calcDisplay.innerText = numberStorageArray.reduce((a, b) => a + b);
-		} else if (currentOperator === '-') {
-			calcDisplay.innerText = numberStorageArray.reduce((a, b) => a - b);
-		} else if (currentOperator === 'X') {
-			calcDisplay.innerText = numberStorageArray.reduce((a, b) => a * b);
-		} else if (currentOperator === '/') {
-			calcDisplay.innerText = numberStorageArray.reduce((a, b) => a / b);
+		switch (currentOperator) {
+			case '+':
+				return (calcDisplay.innerText =
+					parseFloat(numberStorageArray[0]) +
+					parseFloat(numberStorageArray[1]));
+			case '-':
+				return (calcDisplay.innerText =
+					parseFloat(numberStorageArray[1]) -
+					parseFloat(numberStorageArray[0]));
+			case 'X':
+				return (calcDisplay.innerText =
+					parseFloat(numberStorageArray[0]) *
+					parseFloat(numberStorageArray[1]));
+			case '/':
+				return (calcDisplay.innerText =
+					parseFloat(numberStorageArray[1]) /
+					parseFloat(numberStorageArray[0]));
+			default:
+				break;
 		}
+
+		// REPLACE NUMBER ARRAY WITH LAST CALCULUTIOIN
+		// numberStorageArray.splice(0, 2, calcDisplay.innerText);
 	}
 };
