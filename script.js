b1003757cb33c4f1e8d3b818b8a85b88e27bb8db
Victor <victorv24@gmail.com>
2021-04-23 19:23:59 -0700

    Moves JS file to root.

--- script.js
+++ script.js
@@ -4,34 +4,37 @@ let currentOperator = null;
 const calcDisplay = document.getElementById('calc-display');
 
 // -- Listens to Number Buttons
-document.querySelectorAll('#number-button-container').forEach((button) => {
-	button.addEventListener('click', (e) => {
+document.querySelectorAll('#number-button-container').forEach(button => {
+	button.addEventListener('click', e => {
 		calcDisplay.innerText = null;
 		const buttonValue = e.target.textContent;
-		displayNumbers(buttonValue);
+		displayUserNumber(buttonValue);
 	});
 });
 
 // -- Listens to Math Operators
-document.querySelectorAll('#math-operator-container').forEach((button) => {
-	button.addEventListener('click', (e) => {
+document.querySelectorAll('#math-operator-container').forEach(button => {
+	button.addEventListener('click', e => {
 		const buttonValue = e.target.textContent;
 
+		// -- Checks the Number Storage Array to decide what to do
 		if (numberStorageArray.length < 2) {
 			numberStorageArray.push(parseFloat(calcDisplay.innerText));
 		} else if ((numberStorageArray.length = 2)) {
 			const quickCalc = calculate(buttonValue);
-			clearArray();
+			clearNumberStorageArray();
+			debugger;
+			calcDisplay.innerText = quickCalc;
 			numberStorageArray.push(quickCalc);
 		}
 
 		if (buttonValue !== '=' && buttonValue !== 'CE') {
 			currentOperator = buttonValue;
 		} else if (buttonValue === 'CE') {
-			clearArray();
+			clearNumberStorageArray();
 		}
 
-		clearDisplay();
+		clearCalcDisplay();
 
 		if (buttonValue === '=') {
 			calculate();
@@ -62,30 +65,6 @@ const calculate = () => {
 	}
 };
 
-const displayNumbers = (numberValue) => (calcDisplay.innerText += numberValue);
-const clearDisplay = () => (calcDisplay.innerText = '0');
-
-// const calculate = () => {
-// 	switch (currentOperator) {
-// 		case '+':
-// 			return (calcDisplay.innerText =
-// 				parseFloat(numberStorageArray[0]) +
-// 				parseFloat(numberStorageArray[1]));
-// 		case '-':
-// 			return (calcDisplay.innerText =
-// 				parseFloat(numberStorageArray[1]) -
-// 				parseFloat(numberStorageArray[0]));
-// 		case 'X':
-// 			return (calcDisplay.innerText =
-// 				parseFloat(numberStorageArray[0]) *
-// 				parseFloat(numberStorageArray[1]));
-// 		case '/':
-// 			return (calcDisplay.innerText =
-// 				parseFloat(numberStorageArray[1]) /
-// 				parseFloat(numberStorageArray[0]));
-// 		default:
-// 			break;
-// 	}
-// };
-
-const clearArray = () => (numberStorageArray.length = 0);
+const displayUserNumber = numberValue => (calcDisplay.innerText += numberValue);
+const clearCalcDisplay = () => (calcDisplay.innerText = '0');
+const clearNumberStorageArray = () => (numberStorageArray.length = 0);
